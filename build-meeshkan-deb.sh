3f3223bf8f532b6cdce170dbd89b3bd68b42a929
Fredrik Fornwall <fredrik@fornwall.net>
2020-03-12 14:26:26 +0100

    Update build-meeshkan-deb.sh

--- build-meeshkan-deb.sh
+++ build-meeshkan-deb.sh
@@ -4,7 +4,7 @@ set -e -u
 DEB_BUILD_DIR=/root/deb
 rm -Rf $DEB_BUILD_DIR
 
-# Setup /opt/meeshkan inside build directory:
+# Setup /opt/meeshkan:
 MEESHKAN_INSTALL_DIR=/opt/meeshkan
 python3 -m venv $MEESHKAN_INSTALL_DIR
 . $MEESHKAN_INSTALL_DIR/bin/activate
@@ -18,7 +18,7 @@ mkdir -p $DEB_BUILD_DIR/usr/bin
 cd $DEB_BUILD_DIR/usr/bin
 ln -s /opt/meeshkan/bin/meeshkan $DEB_BUILD_DIR/usr/bin/meeshkan
 
-# Create data tarfile:
+# Create data tarfile (containing /opt/meeshkan and /usr/bin/meeshkan):
 cd $DEB_BUILD_DIR
 mkdir opt
 mv /opt/meeshkan opt/meeshkan
