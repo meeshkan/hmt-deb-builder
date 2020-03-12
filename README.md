61b9d72d97b5d064f0bfdc9c621a5d39fb5d4736
Fredrik Fornwall <fredrik@fornwall.net>
2020-03-12 14:05:36 +0100

    Improve deb building

--- README.md
+++ README.md
@@ -1,7 +1,17 @@
 # meeshkan-deb-builder
 Docker image to build a meeshkan deb file.
 
-# How to use
+# How to install the debian package
+Run the following commands:
+
+```sh
+apt install ca-certificates
+echo "deb [trusted=yes] https://dl.bintray.com/meeshkan/apt all main" | sudo tee -a /etc/apt/sources.list
+apt update
+apt install meeshkan
+```
+
+# How to build the debian package
 Run
 
 ```sh
@@ -11,5 +21,3 @@ docker run -v $PWD:/out fredrikfornwall/meeshkan-deb-builder
 to create a deb file (such as `meeshkan-0.2.16.deb`) in the current directory.
 
 It will use the [latest meeshkan release on PyPi](https://pypi.org/project/meeshkan/).
-
-
--- build-meeshkan-deb.sh
+++ build-meeshkan-deb.sh
@@ -5,10 +5,13 @@ DEB_BUILD_DIR=/root/deb
 rm -Rf $DEB_BUILD_DIR
 
 # Setup /opt/meeshkan inside build directory:
-MEESHKAN_INSTALL_DIR=$DEB_BUILD_DIR/opt/meeshkan
+MEESHKAN_INSTALL_DIR=/opt/meeshkan
 python3 -m venv $MEESHKAN_INSTALL_DIR
 . $MEESHKAN_INSTALL_DIR/bin/activate
 pip3 install meeshkan
+MEESHKAN_VERSION=`pip3 show meeshkan | grep Version: | cut -d ' ' -f 2`
+MEESHKAN_INSTALL_SIZE=`du -s $MEESHKAN_INSTALL_DIR`
+deactivate
 
 # Setup /usr/bin/meeshkan symlink inside build directory:
 mkdir -p $DEB_BUILD_DIR/usr/bin
@@ -17,12 +20,12 @@ ln -s /opt/meeshkan/bin/meeshkan $DEB_BUILD_DIR/usr/bin/meeshkan
 
 # Create data tarfile:
 cd $DEB_BUILD_DIR
+mkdir opt
+mv /opt/meeshkan opt/meeshkan
 tar -cJf "/root/data.tar.xz" -H gnu .
 mv /root/data.tar.xz $DEB_BUILD_DIR/
 
 # Create debian control file:
-MEESHKAN_VERSION=`pip3 show meeshkan | grep Version: | cut -d ' ' -f 2`
-MEESHKAN_INSTALL_SIZE=`du -s $MEESHKAN_INSTALL_DIR`
 mkdir -p $DEB_BUILD_DIR/DEBIAN
 cat > $DEB_BUILD_DIR/DEBIAN/control <<EOF
 Package: meeshkan
